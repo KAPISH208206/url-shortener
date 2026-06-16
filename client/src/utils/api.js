6f10be653f8f4d34a43e7261e3a4b234312a64c7
Kapish Gupta <kapish.guptahsr@gmail.com>
2026-06-16 19:58:08 +0530

    api changes and ready for deployment

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "/api/url" });
+const API = axios.create({ 
+  baseURL: process.env.REACT_APP_API_URL || "/api/url" 
+});
 
 export const shortenUrl = (originalUrl) => API.post("/shorten", { originalUrl });
 export const getAllUrls = () => API.get("/all");
