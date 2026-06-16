e36a7cc34043792b04ad77e78ce1ef78cbf3c98b
Kapish Gupta <kapish.guptahsr@gmail.com>
2026-06-16 20:18:49 +0530

    cors added

--- server/index.js
+++ server/index.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:3000" }));
+app.use(cors({
+  origin: ["https://url-shortener-flax-pi.vercel.app", "http://localhost:3000"]
+}));
 app.use(express.json());
 
 app.use("/api/url", urlRoutes);
